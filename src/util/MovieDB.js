e5927d25b0d22d836c3a94bc3a5e58a49ceeabaf
Freddy Bruce <freddybruce@freddys-mbp-2.home>
2023-06-07 11:31:30 +0100

    updating env variables for railway

--- src/util/MovieDB.js
+++ src/util/MovieDB.js
@@ -2,7 +2,7 @@ const options = {
   method: 'GET',
   headers: {
     accept: 'application/json',
-    Authorization: `Bearer ${process.env.MOVIEDB_API_KEY}`
+    Authorization: `Bearer ${process.env.REACT_APP_MOVIEDB_API_KEY}`
   }
 };
 
